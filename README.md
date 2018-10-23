0b8baf2020f1f8a9fa65872900937a7e8d252890
matalus <matt.hamende@gmail.com>
2018-10-23 00:18:14 -0700

    docker test initial commit

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 execute server.ps1 to start server by default listens on port 8000, can be reconfigured by changing $ListenPort variable in script
 
-to launch in docker run "docker run -p 8000:8000 <imagename>"
+to launch in docker execute: 
+    "docker build -t psrest ."
+    "docker run -p 8000:8000 -h psrest --name ps_serv psrest"
 
 Commands:
 
---
+++ dockertest.ps1
@@ -0,0 +1,14 @@
+﻿
+While($true){
+    $seed = @("starwars","topgun","flavortown")[(Get-Random -maximum 3)]; 
+    $dockerstat = (docker inspect psrest | ConvertFrom-Json)
+    $quote = (Invoke-RestMethod -Method GET -Uri "http://localhost:8000/$seed").body.table.tr.th.'#text' 
+    $time = New-TimeSpan -Start ([datetime]$dockerstat.State.StartedAt) -End (get-date)
+    $timestr = if($time.hours -lt 1){
+        "$([math]::Round($time.TotalMinutes,1)) Minutes"
+    }else{
+        "$([math]::Round($time.TotalHours,1)) Hours"
+    }
+    "seed: $seed | docker: up $timestr | $quote"
+    Start-Sleep -Seconds 5 
+}
